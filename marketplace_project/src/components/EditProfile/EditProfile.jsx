6db5c56e4c229f1e3b1fe861c2bbb462b41ac8bc
حبيبة مصطفى محمود قناوى <143437900+Habibakenawy@users.noreply.github.com>
2025-05-11 01:04:00 +0300

    Update EditProfile.jsx

--- marketplace_project/src/components/EditProfile/EditProfile.jsx
+++ marketplace_project/src/components/EditProfile/EditProfile.jsx
@@ -30,17 +30,52 @@ function EditProfile() {
         </nav>
       </div>
       <div className='container-fluid' style={{ backgroundColor: "#0A0615", height: "200vh" }}>
-      <div className='container'>
-      <div className='row'>
+      <div className='container pt-5'>
+      <div className='row'> 
      <div className='col-sm-2'>
+     <div className='img_container ' style={{backgroundColor:'#201F2D'}}>
      <div className='img'>
      <img src={authorImg}
-      className="img-fluid rounded-start "
+      className="img-fluid rounded-start rounded-3 w-100 d-block "
       alt="User"/>
      </div>
+     <button type="button" className="btn btn-outline-secondary  text-white rounded-4  border border-2 m-3 ">Upload New Photo</button>
      </div>
-     <div className='col-sm-10'>
-
+     </div>
+     <div className='col-sm-5'>
+      <h2 className='text-white'>Account Info</h2>
+      <div className="mb-3">
+  <label for="exampleFormControlInput1" className="form-label text-white">Name</label>
+  <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="Name"/>
+</div>
+  <div className="mb-3">
+  <label for="exampleFormControlInput1" className="form-label text-white">Email address</label>
+  <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com"/>
+</div>
+  <div className="mb-3">
+  <label for="exampleFormControlInput1" className="form-label text-white">Phone Number</label>
+  <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="+20 109 888 999"/>
+</div>
+<div className="mb-3">
+  <label for="exampleFormControlTextarea1" className="form-label text-white">bio</label>
+  <textarea className="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+</div>
+     <button type="button" className="btn btn-outline-secondary  text-white rounded-4  border border-2 ">Upload New Photo</button>
+     </div>
+      <div className='col-sm-5'>
+      <h2 className='text-white'>Your Social Media</h2>
+        <div className="mb-3">
+  <label for="exampleFormControlInput1" className="form-label text-white">facebook link</label>
+  <input type="email" class="form-control" id="exampleFormControlInput1" placeholder=""/>
+</div>
+  <div className="mb-3">
+  <label for="exampleFormControlInput1" className="form-label text-white">twitter link</label>
+  <input type="email" class="form-control" id="exampleFormControlInput1" placeholder=""/>
+</div>
+  <div className="mb-3">
+  <label for="exampleFormControlInput1" className="form-label text-white">discord link</label>
+  <input type="email" class="form-control" id="exampleFormControlInput1" placeholder=""/>
+</div>
      </div>
       </div>
       </div>
